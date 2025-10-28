524073f0c8ac702946c2c0a5e37f7cdf6aeb803b
Kane Frayzy <kanefrayzy@gmail.com>
2025-10-29 02:45:23 +0500

    Fix

--- wp-content/plugins/gs-stripe-gateway/include/settings.php
+++ wp-content/plugins/gs-stripe-gateway/include/settings.php
@@ -1,34 +1,3 @@
 <?php
 
-// פונקציה מותאמת להצפנה לפני שמירה בבסיס הנתונים
-function custom_tools_sanitize_key($input) {
-    // אם השדה לא ריק, הצפן את המפתח
-    if (!empty($input)) {
-        // דאג להצפנה רק אם הערך לא כבר base64
-        if (!preg_match('/^[A-Za-z0-9+\/=]*$/', $input)) {
-            return base64_encode($input); // הצפנת המפתח
-        }
-    }
-    return $input; // אם כבר מוצפן, נשאיר אותו ככה
-}
-
-// סינון המפתח לפני השמירה בבסיס הנתונים
-add_filter('pre_update_option_product_key_field_prod', 'custom_tools_sanitize_key');
-add_filter('pre_update_option_product_key_field_dev', 'custom_tools_sanitize_key');
-add_filter('pre_update_option_public_key_field_prod', 'custom_tools_sanitize_key');
-add_filter('pre_update_option_public_key_field_dev', 'custom_tools_sanitize_key');
-
-// פונקציה לפענוח המפתח לפני הצגתו
-function custom_tools_unsanitize_key($value) {
-    // אם הערך לא ריק ופענח אותו
-    if (!empty($value)) {
-        return base64_decode($value); // פענוח המפתח
-    }
-    return $value;
-}
-
-// פענוח המפתח כאשר הוא יוצג בשדות
-add_filter('option_product_key_field_prod', 'custom_tools_unsanitize_key');
-add_filter('option_product_key_field_dev', 'custom_tools_unsanitize_key');
-add_filter('option_public_key_field_prod', 'custom_tools_unsanitize_key');
-add_filter('option_public_key_field_dev', 'custom_tools_unsanitize_key');
+// Base64 encoding/decoding temporarily disabled
